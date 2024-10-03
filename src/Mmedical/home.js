fa1f07702ce940c691d9d497bd112e4df1f2fc6c
Camilo Abarca <camilo.abarca.21@gmail.com>
2024-10-03 10:24:23 -0300

    Se agrga funcionalidad para "Ver"

--- src/Mmedical/home.js
+++ src/Mmedical/home.js
@@ -16,6 +16,7 @@ const personaContacto = document.getElementById('personaContacto')
 const fono = document.getElementById('fono')
 const email = document.getElementById('email')
 
+//Función para agregar equipos a la tabla
 function getEquipos() {
   fetch('https://mmedical-api-d691032dcc94.herokuapp.com/api/v1/equipos/', {
     method: 'GET',
@@ -45,7 +46,7 @@ function getEquipos() {
         button.type = 'button';
         button.className = 'btn btn-primary';
         button.textContent = 'Ver';
-        button.addEventListener('click', () => verEquipo(equipo.id));
+        button.addEventListener('click', () => verEquipo(equipo));
         buttonCell.appendChild(button);
       });
     })
@@ -53,7 +54,7 @@ function getEquipos() {
 }
 
 
-
+//Función para formatear fechas
 function formatDate(dateString) {
   const date = new Date(dateString);
   return date.toLocaleDateString('es-ES', {
@@ -63,11 +64,52 @@ function formatDate(dateString) {
   });
 }
 
-function verEquipo(id) {
-  console.log('Se está viendo', id)
+//Función para ver detalles del equipo
+function verEquipo(equipo) {
+  var viewModal = document.getElementById('viewModal');
+  var modal = new bootstrap.Modal(viewModal);
+  modal.show();
+
+  var equipoData = getEquipoData(equipo);
+  document.getElementById('id-view').innerHTML = equipoData.id
+  document.getElementById('marca-view').innerHTML = equipoData.marca
+  document.getElementById('modelo-view').innerHTML = equipoData.modelo
+  document.getElementById('fIngreso-view').innerHTML = equipoData.fIngreso
+  document.getElementById('fEntrega-view').innerHTML = equipoData.fEntrega
+  document.getElementById('fMantencion-view').innerHTML = equipoData.fMantencion
+  document.getElementById('detalle-view').innerHTML = equipoData.detalle
+  document.getElementById('otroDetalle-view').innerHTML = equipoData.otroDetalle
+  document.getElementById('accesorios-view').innerHTML = equipoData.accesorios
+  document.getElementById('estado-view').innerHTML = equipoData.estado
+  document.getElementById('razonSocial-view').innerHTML = equipoData.razonSocial
+  document.getElementById('centroMedico-view').innerHTML = equipoData.centroMedico
+  document.getElementById('personaContacto-view').innerHTML = equipoData.personaContacto
+  document.getElementById('fono-view').innerHTML = equipoData.fono
+  document.getElementById('email-view').innerHTML = equipoData.email
 }
 
+function getEquipoData(equipo) {
+  var equipoData = {
+    id: 'ID: ' + equipo.id,
+    marca: 'Marca: ' + equipo.marca,
+    modelo: 'Modelo: ' + equipo.modelo,
+    fIngreso: 'Fecha Ingreso: ' + formatDate(equipo.fechaIngreso),
+    fEntrega: 'Fecha Entrega: ' + formatDate(equipo.fechaEntrega),
+    fMantencion: 'Fecha Mantencion: ' + formatDate(equipo.fechaMantencion),
+    detalle: 'Detalle: ' + equipo.detalle,
+    otroDetalle: 'Otro: ' + equipo.otroDetalle,
+    accesorios: 'Accesorios: ' + equipo.accesorios,
+    estado: 'Estado: ' + equipo.estado,
+    razonSocial: 'Razón Social: ' + equipo.razonSocial,
+    centroMedico: 'Centro Medico: ' + equipo.centroMedico,
+    personaContacto: 'Persona Contacto: ' + equipo.personaContacto,
+    fono: 'Fono: ' + equipo.fono,
+    email: 'Email: ' + equipo.email
+  };
+  return equipoData;
+}
 
+//Agrega equipos
 function agregarEquipo(event) {
   event.preventDefault()
   const formFields =
